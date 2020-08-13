6f501078db6250275af0adc99eade59e9da8486f
Elias-Cz <cazessus.elias@gmail.com>
2020-08-13 10:47:41 -0700

    gallery ajax req

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,17 @@ function loadDoc2() {
   xhttp.send();
 }
 
+function loadDoc3() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("switch").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "about_info.txt", true);
+  xhttp.send();
+}
+
 
 document.addEventListener('click', event => {
   const element = event.target;
